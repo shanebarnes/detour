c2c18519e4aa1d08104083359e6d80e8f54e51bc
shanebarnes <shanebarnes@users.noreply.github.com>
2017-05-16 22:29:28 -0400

    Allow  and  to be set from command line variables

--- build.sh
+++ build.sh
@@ -4,14 +4,24 @@ set -e
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export GOPATH="$script_dir"
-export GOBIN="${GOPATH}/bin"
+#export GOBIN="${GOPATH}/bin"
 #go env
 
-cd "$GOPATH"
-mkdir -p "$GOBIN"
+# E.g., linux
+if [ ! -z "${1}" ]; then
+    export GOOS="${1}"
+fi
 
-printf "Downloading and installing packages and dependencies...\n"
-go get ./...
+# E.g., amd64
+if [ ! -z "${2}" ]; then
+    export GOARCH="${2}"
+fi
+
+cd "$GOPATH"
+#mkdir -p "$GOBIN"
+
+#printf "Downloading and installing packages and dependencies...\n"
+#go get ./...
 
 printf "Compiling packages and dependencies...\n"
 go build -ldflags -s
