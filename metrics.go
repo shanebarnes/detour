0505962ae84289fd9bf442a47c40a10939cf68e5
shanebarnes <shanebarnes@users.noreply.github.com>
2018-03-01 22:48:38 -0500

    Removing glide and restructring project

---
+++ metrics.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Metrics struct {
+	logger        *log.Logger
+	timeStartNs   int64
+	timeReportNs  int64 // Last report time
+	byteReport    int64
+	reportIntNs   int64 // Report interval in nanoseconds
+	reportIntByte int64
+	byteCount     int64
+	tag           string
+	buffer        []string
+}
+
+func MetricsNew(logger *log.Logger, reportIntNs int64, reportIntByte int64, tag string) *Metrics {
+	metrics := new(Metrics)
+	metrics.logger = logger
+	metrics.timeStartNs = time.Now().UnixNano()
+	metrics.timeReportNs = metrics.timeStartNs
+	metrics.reportIntNs = reportIntNs
+	metrics.reportIntByte = reportIntByte
+	metrics.tag = tag
+	metrics.byteReport = 0
+	metrics.byteCount = 0
+	metrics.Add(0)
+	return metrics
+}
+
+func (m *Metrics) Add(bytes int64) {
+	now := time.Now().UnixNano()
+	m.byteCount = m.byteCount + bytes
+	m.byteReport = m.byteReport + bytes
+
+	if (m.reportIntNs > 0 && now >= m.timeReportNs) || (m.reportIntByte > 0 && m.byteReport >= m.reportIntByte) {
+		avgBps := float64(m.byteCount) * 8. * 1000000000. / float64(now-m.timeStartNs)
+		m.buffer = append(m.buffer, fmt.Sprintf("%.6f,%s,%d,%d,%d,%.6f", float64(now)/1000000000., m.tag, (now-m.timeStartNs)/1000, bytes, m.byteCount, avgBps/1000000.))
+
+		m.byteReport = 0
+		for m.timeReportNs <= now {
+			m.timeReportNs = m.timeReportNs + m.reportIntNs
+		}
+	}
+}
+
+func (m *Metrics) Dump() {
+	m.timeReportNs = time.Now().UnixNano()
+	m.Add(0)
+
+	for _, record := range m.buffer {
+		m.logger.Println(record)
+	}
+}
